38c674d459415f5c7cb034df99e36502597f11b2
alexandr <frexeman2001@yandex.ru>
2022-03-10 23:37:11 +0300

    Задание task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,5 +1,15 @@
 export function romanToInteger(str) {
     let result = 0;
+    let comparison = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
+    let ints = Object.keys(comparison);
+    str = str.toUpperCase();
+    for(let i=0; i<str.length;i++){
+        if(ints.indexOf(str[i])<ints.indexOf(str[i+1])){
+            result -=comparison[str[i]];
+        } else{
+            result +=comparison[str[i]];
+        }
+    }
     /*
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
